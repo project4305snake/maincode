7173875283f580a66f8497698caa08dea93abb54
LeHuyHoang <pronbk96@gmail.com>
2017-07-16 08:58:03 +0700

    core code

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace snake_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(484, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bấm cách để bắt đầu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hướng dẫn";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 251);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_game
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        Random randFood = new Random();
+        food food;
+        Graphics paper;
+        snake snake = new snake();
+
+        public Form1()
+        {
+            InitializeComponent();
+            food = new food(randFood);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+         toolStripLabel1.Text = score.ToString();
+            if (down == true) { snake.moveDown(); }
+            if (up == true) { snake.moveUp(); }
+            if (left == true) { snake.moveLeft(); }
+            if (right == true) { snake.moveRight(); }
+            for (int i = 0; i < snake.SnakeRec.Length; i++)
+            {
+                if (snake.SnakeRec[i].IntersectsWith(food.foodRec))
+                {
+                    Random rd = new Random();
+                    score += rd.Next(5, 30);
+                    snake.growSnake();
+                    food.foodLocation(randFood);
+                }
+            }
+            collision();
+            this.Invalidate();
+        }
+        void Restart()
+        {
+            timer2.Enabled = true;
+            label1.Text = "Bấm Phím Cách Để Bắt Đầu Chơi";
+           toolStripLabel1.Text = "0";
+            score = 0;
+            MessageBox.Show("Game Over!!!");
+            snake = new snake();
+        }
+        public void collision()
+        {
+            for (int i = 1; i < snake.SnakeRec.Length; i++)
+            {
+                if (snake.SnakeRec[0].IntersectsWith(snake.SnakeRec[i]))
+                {
+                    timer2.Enabled = false;
+                    MessageBox.Show("Bạn đã tự cắn đuôi");
+
+                    label1.Text = "Bấm Phím Cách Để Bắt Đầu Chơi";
+                   toolStripLabel1.Text = "0";
+                    score = 0;
+                    MessageBox.Show("Game Over!!!");
+                    snake = new snake();
+                    Restart();
+                }
+                if (snake.SnakeRec[0].Y < 0 || snake.SnakeRec[0].Y > 290)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Bạn đã cắn vào tường ");
+
+                    label1.Text = "Bấm Phím Cách Để Bắt Đầu Chơi";
+                    toolStripLabel1.Text = "0";
+                    score = 0;
+                    MessageBox.Show("Game Over!!!");
+                    snake = new snake();
+                    Restart();
+                }
+                if (snake.SnakeRec[0].X < 0 || snake.SnakeRec[0].X > 500)
+                {
+                    timer1.Enabled = false;
+                    MessageBox.Show("Bạn đã cắn vào tường");
+                    label1.Text = "Bấm Phím Cách Để Bắt Đầu Chơi";
+                    toolStripLabel1.Text = "0";
+                    score = 0;
+                    MessageBox.Show("Game Over!!!");
+                    snake = new snake();
+                    Restart();
+                }
+            }
+        }
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Boolean left = false, right = false, up = false, down = false;
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            MessageBox.Show("Phần mềm được phát triển bởi:\r" +
+                "Nhóm dhcn1a\r" +
+                "--------------------------\r" +
+                "HƯỚNG DẪN CÁCH CHƠI:\r" +
+                "Bạn nhấn phím cách (Space) để bắt đầu chơi.\r"
+                + "Sau đó hãy nhấn các phím trái phải lên xuống\r" +
+                "để định hướng cho chú rắn của bạn chạy.\r" +
+                "Có 5 mức độ cho bạn chọn:\r" +
+                "Ấn phím số 1 để chọn chế độ dể.\r"
+                + "Ấn phím số 2 để chọn chế độ bình thường.\r" +
+                "Ấn phím số 3 để chọn chế độ khó.\r" +
+                "Ấn phím số 4 để chọn chế độ siêu khó.\r" +
+                "Ấn phím số 5 để chọn chế độ thánh thức thần thánh", "Hướng Dẫn");
+            label1.Text = "Ấn Phím Cách Để Tiếp Tục . . .";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            paper = e.Graphics;
+            food.drawFood(paper);
+            snake.drawSnake(paper);
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.D1)
+            {
+                timer2.Interval = 500;
+            }
+            if (e.KeyData == Keys.D2)
+            {
+                timer2.Interval = 300;
+            }
+            if (e.KeyData == Keys.D3)
+            {
+                timer2.Interval = 100;
+            }
+            if (e.KeyData == Keys.D4)
+            {
+                timer2.Interval = 50;
+            }
+            if (e.KeyData == Keys.D5)
+            {
+                timer2.Interval = 10;
+            }
+            if (e.KeyData == Keys.Space)
+            {
+                timer2.Enabled = true;
+                left = false; right = false; up = false; down = false;
+                label1.Text = "";
+            }
+            if (e.KeyData == Keys.Up && down == false)
+            {
+                up = true;
+                down = false;
+                left = false;
+                right = false;
+            }
+            if (e.KeyData == Keys.Down && up == false)
+            {
+                up = false;
+                down = true;
+                left = false;
+                right = false;
+            }
+            if (e.KeyData == Keys.Left && right == false)
+            {
+                up = false;
+                down = false;
+                left = true;
+                right = false;
+            }
+            if (e.KeyData == Keys.Right && left == false)
+            {
+                up = false;
+                down = false;
+                left = false;
+                right = true;
+            }
+        }
+    }
+}
